50730bdc537c88b4db6f3869efc795bdf7df4809
kranthivitta123 <kranthireddy.v30@gmail.com>
2021-10-16 17:10:04 +0530

    Create

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req , res , next) => {
-    if(!req.body.name) {
-      return  res.status(404).json({
-            status:"Name not found"
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name) {
+        return res.status(404).json({
+            status: "Name not found"
         })
     }
     next();
@@ -12,40 +14,32 @@ exports.checkBody = (req , res , next) => {
 
 exports.getAllTours = (req, res) => {
     res.status(200).json({
-        message: "success",
-        count: tours.length,
-        requestedAt:req.requestedTime,
-        data: tours
+        message: "success"
     })
 }
 
 exports.getTour = (req, res) => {
     const id = req.params.id;
-    const obj = tours.find(item => item.id == id);
-    if (obj) {
-        res.status(200).json({
-            status: "success",
-            data: obj
-        })
-    }
-    else {
-        res.status(204).json({
-            message: "Not found"
-        })
-    }
+    res.status(200).json({
+        status: "success",
+    })
 }
 
-exports.addTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body)
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), suc => {
+exports.addTour = async (req, res) => {
+    try {
+        const tour = new Tour(req.body);
+        const newTour = await tour.save();
         res.status(200).json({
             message: "success",
             data: newTour
         })
-    })
-}
+    } catch (err) {
+        res.status(400).json({
+            status: 'error',
+            message: err
+        })
+    }
+};
 
 exports.updateTour = (req, res) => {
     res.status(200).json({
@@ -54,18 +48,9 @@ exports.updateTour = (req, res) => {
     })
 }
 
-exports.deleteTour =  (req, res) => {
-    const id = req.params.id;
-    const obj = tours.find(item => item.id == id);
-    if (obj) {
-        res.status(200).json({
-            status: "success",
-            data: obj
-        })
-    }
-    else {
-        res.status(204).json({
-            message: "null"
-        })
-    }
+exports.deleteTour = (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: obj
+    })
 }
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose')
 const env = require('dotenv')
 env.config({path:'./config.env'})
 
 const app = require('./app')
+const DB = process.env.DATABASE.replace('<PASSWORD>' , process.env.DATABASE_PASSWORD)
 
 // console.log(process.env)
+
+mongoose.connect(DB ,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+}).then(con => {
+    console.log("Connection Successfull")
+})
+
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
