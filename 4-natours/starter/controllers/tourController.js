3bd056ed68a560f88f66f16adf19566b7ba70403
kranthi <kranthireddy.v30@gmail.com>
2021-10-22 15:42:31 +0530

    added Filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -14,7 +14,11 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        const queryObj = {...req.query};
+        const excludeFields = ['page','sort','limit','fields']
+        excludeFields.forEach(el => delete queryObj[el]);
+        const query = Tour.find(queryObj);
+        const tours = await query;
         res.status(200).json({
             message: "success",
             count:tours.length,
