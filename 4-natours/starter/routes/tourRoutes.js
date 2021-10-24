fd4e43b609990cfdf65516393f01f3b9b3902fab
kranthi <kranthireddy.v30@gmail.com>
2021-10-24 12:53:27 +0530

    Aggregations && ApiFeatures

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -5,10 +5,16 @@ const tourController = require('../controllers/tourController');
 //only for this route middleware
 router.param('id', (req, res, next, val) => {
     console.log("Can be used as common method to check if id is valid");
-    next()
+    next();
 })
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours , tourController.getAllTours)
+
+router.route('/tour-stats')
+   .get(tourController.getAllTourStats)
+
+router.route('/monthly-plan/:year')
+   .get(tourController.getMonthlyPlan)
 
 router.route('/').get(tourController.getAllTours).post(tourController.checkBody , tourController.addTour)
 router.route('/:id').get(tourController.getTour).
