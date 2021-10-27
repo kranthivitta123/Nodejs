8a7c35c8225d232d564c09c56ec84e63a95ac777
kranthi <kranthireddy.v30@gmail.com>
2021-10-27 22:15:17 +0530

    INdent

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -48,11 +48,11 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-userSchema.pre(/^find/, function(next) {
+userSchema.pre(/^find/, function (next) {
     // this points to the current query
     this.find({ active: { $ne: false } });
     next();
-  });
+});
 
 userSchema.pre('save', async function (next) {
     // Only run this function if password was actually modified
